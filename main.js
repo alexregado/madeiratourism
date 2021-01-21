e2979e6e109341f79a817bcf053903d7b305e859
alexregado <70161994+alexregado@users.noreply.github.com>
2021-01-21 17:44:23 +0000

    Finished Website

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
+var nav = document.getElementById('nav');
+
+window.onscroll = function(){
+
+   if (window.pageYOffset >100) {
+
+       nav.style.background = "rgba(139, 139, 139, 0.9)";
+       nav.style.boxShadow = "0px 4px 2px rgb(194, 55, 55)"
+   }
+   else{
+       nav.style.background = "rgba(139, 139, 139, 0.7)";
+       nav.style.boxShadow = "none";
+   }
+}
+
+
+
+
+
+/* IMPORTAR O FORMULÁRIO */
+const myForm = document.getElementById('myForm');
+
+/* SUBMISSÃO DO FORM SEM IR PARA O FICHEIRO PHP */
+myForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const request = new XMLHttpRequest();
+    request.open("post", "http://localhost/php/Trabalho/forms.php");
+    request.onload = function () {
+        console.log(request.responseText);
+    }
+    request.send(new FormData(myForm));
+    /* MOSTRA NA CONSOLA QUE OS DADOS FORAM SUBMETIDOS E É LANÇADO UM ALERTA */
+    console.log("Form has been submited!");
+    alert('O formulário foi submetido com sucesso! Obrigado pela sua participação e boa sorte!')
+
+});
+
+
+
+
+
 let container = document.querySelector(".CTA");
 const randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 container.addEventListener('click', () => {
     container.style.backgroundColor = randomColor;
 });
+
+
